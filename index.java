cbe3a5b6aae60c3f3f64f84615311ae72e0df700
karimBdot <basskarim90@gmail.com>
2020-09-29 14:07:21 +0000

    eyescand legs

--- index.java
+++ index.java
@@ -8,27 +8,27 @@ void draw() {
   ellipse(mouseX, mouseY, 1, 1);
   println(mouseX, mouseY);
 
-  background(200);
+  background(255);
   // Set CENTER mode
   ellipseMode(CENTER);
   rectMode(CENTER);
   // Draw Zoog's body
   stroke(0);
   fill(150);
-  rect(mouseX, mouseY,20,100);
+  rect(mouseX, mouseY,40,100);
   // Draw Zoog's head
   stroke(0);
   fill(255);
-  ellipse(mouseX, mouseY - 30,60,60);
-  // Draw Zoog's eyes
+  ellipse(mouseX, mouseY - 30,70,70);
   fill(0);
+  stroke(0)
 
   // remove the forward slashes in front of each line of code below one at a time. Remove the underline and replace with the proper code to allow your mouse to move Zoog around  💻
-
-  //ellipse(__________, __________,16,32);
-  //ellipse(__________, __________,16,32);
+  fill(0)
+  ellipse(mouseX -10,mouseY -30,16,32);
+  ellipse(mouseX +10,mouseY -30,16,32);
   // Draw Zoog's legs
   stroke(0);
-  //line(_________, __________, __________, __________);
-  //line(__________, __________, __________, __________);
-}
+    line(mouseX -10, mouseY +50, mouseX -20, mouseY +70);
+    line(mouseX +10, mouseY +50, mouseX +20, mouseY +70);
+   }
